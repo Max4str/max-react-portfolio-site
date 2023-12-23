d7a632fd4bc46f6398ca48bed84589e8346492a5
Max Foerster <max4str@gmail.com>
2023-12-23 21:38:14 +1100

    initial commit

---
+++ Notes.md
@@ -0,0 +1,7 @@
+Tutorial: https://www.youtube.com/watch?v=G-Cr00UYokU&list=WL&t=372
+
+React Icons: http://react-icons.github.io/react-icons/
+
+SwiperJS: https://swiperjs.com/
+
+EmailJS: https://www.emailjs.com/
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,86 @@
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_4vpyt7d",
+      "template_mwonf7k",
+      form.current,
+      "72-6ivKamSaSYvZO4"
+    );
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>max4str@gmail.com</h5>
+            <a href="mailto:max4str@gmail.com" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Max Foerster</h5>
+            <a
+              href="https://m.me/maximillian.foerster/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>max4str@gmail.com</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+610484133994"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message Here"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+export default Contact;
